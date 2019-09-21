10ed3a37b43455d1f8b23a3e892b5bfbb07a1e27
Ferdinand <fercalo96@yahoo.de>
2019-09-21 14:13:52 +0200

    Improved Swiches/Levers Model.

--- src/main/java/net/fexcraft/mod/addon/pack5pxt/models/gauges/StandardRail.java
+++ src/main/java/net/fexcraft/mod/addon/pack5pxt/models/gauges/StandardRail.java
@@ -3,13 +3,11 @@ package net.fexcraft.mod.addon.pack5pxt.models.gauges;
 
 import net.fexcraft.lib.common.math.Vec3f;
 import net.fexcraft.lib.mc.api.registry.fModel;
-import net.fexcraft.lib.tmt.ModelBase;
 import net.fexcraft.lib.tmt.ModelRendererTurbo;
 import net.fexcraft.mod.fvtm.entity.JunctionSwitchEntity;
 import net.fexcraft.mod.fvtm.model.RailGaugeModel;
 import net.fexcraft.mod.fvtm.model.TurboList;
 import net.fexcraft.mod.fvtm.sys.rail.Junction;
-import net.minecraft.util.ResourceLocation;
 
 /** This file was exported via the FVTM Exporter V1.3 of<br>
  *  FMT (Fex's Modelling Toolbox) v.1.2.9 &copy; 2019 - Fexcraft.net<br>
@@ -17,9 +15,6 @@ import net.minecraft.util.ResourceLocation;
  */
 @fModel(registryname = "5pxt:models/gauges/standard")
 public class StandardRail extends RailGaugeModel {
-	
-	private TurboList single_switch;
-	private static final ResourceLocation sstex = new ResourceLocation("pack5pxt:textures/rails/single_switch.png");//TEMPORARY
 
 	public StandardRail(){
 		super(); textureX = 16; textureY = 16;
@@ -48,20 +43,74 @@ public class StandardRail extends RailGaugeModel {
 		);
 		this.groups.add(ties);
 		//
-		single_switch = new TurboList("single_switch");
-		single_switch.add(new ModelRendererTurbo(single_switch, 2, 3, 16, 8).addBox(0, 0, 0, 3, 1, 4)
+		double_base = new TurboList("double_base");
+		double_base.add(new ModelRendererTurbo(double_base, 9, 5, textureX, textureY).addBox(0, 0, 0, 6, 1, 4)
+			.setRotationPoint(-3, -1, -2).setRotationAngle(0, 0, 0)
+		);
+		this.groups.add(double_base);
+		//
+		double_lever0 = new TurboList("double_lever0");
+		double_lever0.add(new ModelRendererTurbo(double_lever0, 11, 11, textureX, textureY).addBox(0, -1.5f, -0.5f, 2, 2, 1)
+			.setRotationPoint(-2.5f, -1, 0).setRotationAngle(0, 0, 0)
+		);
+		this.groups.add(double_lever0);
+		//
+		double_lever1 = new TurboList("double_lever1");
+		double_lever1.add(new ModelRendererTurbo(double_lever1, 20, 0, textureX, textureY).addBox(0, -1.5f, -0.5f, 2, 2, 1)
+			.setRotationPoint(0.5f, -1, 0).setRotationAngle(0, 0, 0)
+		);
+		this.groups.add(double_lever1);
+		//
+		fork2_base = new TurboList("fork2_base");
+		fork2_base.add(new ModelRendererTurbo(fork2_base, 15, 11, textureX, textureY).addBox(0, 0, 0, 3, 1, 4)
 			.setRotationPoint(-1.5f, -1, -2).setRotationAngle(0, 0, 0)
 		);
-		single_switch.add(new ModelRendererTurbo(single_switch, 0, 0, 16, 8).addBox(0, -1.5f, -0.5f, 2, 2, 1)
+		this.groups.add(fork2_base);
+		//
+		fork2_lever = new TurboList("fork2_lever");
+		fork2_lever.add(new ModelRendererTurbo(fork2_lever, 13, 0, textureX, textureY).addBox(0, -1.5f, -0.5f, 2, 2, 1)
 			.setRotationPoint(-1, -1, 0).setRotationAngle(0, 0, 0)
 		);
-		this.groups.add(single_switch);
+		this.groups.add(fork2_lever);
+		//
+		fork3_base = new TurboList("fork3_base");
+		fork3_base.add(new ModelRendererTurbo(fork3_base, 0, 11, textureX, textureY).addBox(0, 0, 0, 3, 1, 4)
+			.setRotationPoint(-1.5f, -1, -2).setRotationAngle(0, 0, 0)
+		);
+		this.groups.add(fork3_base);
+		//
+		fork3_lever = new TurboList("fork3_lever");
+		fork3_lever.add(new ModelRendererTurbo(fork3_lever, 0, 0, textureX, textureY).addCylinder(0, 0, 0, 3, 2, 3, 0.5f, 0.5f, 3, null)
+			.setRotationPoint(-1, -1, 0).setRotationAngle(0, 0, 0)
+		);
+		this.groups.add(fork3_lever);
 	}
 	
 	@Override
 	public void renderSwitch(JunctionSwitchEntity entity, Junction junction){
-		single_switch.get(1).rotationAngleX = junction.switch0 ? -80 : 80;
-		ModelBase.bindTexture(sstex); single_switch.renderPlain();
+		switch(junction.type){
+			case DOUBLE:{
+				double_base.renderPlain();
+				double_lever0.get(0).rotationAngleX = junction.switch0 ? -80 : 80;
+				double_lever0.renderPlain();
+				double_lever1.get(0).rotationAngleX = junction.switch1 ? -80 : 80;
+				double_lever1.renderPlain();
+				break;
+			}
+			case FORK_2:{
+				fork2_base.renderPlain();
+				fork2_lever.get(0).rotationAngleX = junction.switch0 ? -80 : 80;
+				fork2_lever.renderPlain();
+				break;
+			}
+			case FORK_3:{
+				fork3_base.renderPlain();
+				fork3_lever.get(0).rotationAngleX = junction.switch0 ? -120 : junction.switch1 ? 120 : 0;
+				fork3_lever.renderPlain();
+				break;
+			}
+			case CROSSING: case STRAIGHT: default: return;
+		}
 	}
 
 }
