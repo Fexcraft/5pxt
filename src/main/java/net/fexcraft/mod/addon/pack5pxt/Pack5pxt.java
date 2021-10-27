cfe2aac15cbef775d303c9545963e7c43faa0317
Ferdinand Calo <fercalo96@yahoo.de>
2021-10-27 22:23:28 +0200

    1.3.0

--- src/main/java/net/fexcraft/mod/addon/pack5pxt/Pack5pxt.java
+++ src/main/java/net/fexcraft/mod/addon/pack5pxt/Pack5pxt.java
@@ -9,6 +9,6 @@ public class Pack5pxt {
 	
     public static final String MODID = "pack5pxt";
     public static final String NAME = "5PX Gauge Trains Pack";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.0";
     
 }
